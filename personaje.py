9a662e1c838d4b2746657239d09fe9d1812923ad
Nico <nikobazan@hotmail.com>
2015-11-18 15:53:48 -0300

    Agregamos fisicas

--- personaje.py
+++ personaje.py
@@ -4,23 +4,23 @@ import pilasengine
 from pilasengine.habilidades import Habilidad
 
 pilas = pilasengine.iniciar()
+pilas.fisica.gravedad_x=0
+pilas.fisica.gravedad_y=0
 
 class JohnCena(pilasengine.actores.Actor):
 
     def iniciar(self):
         self.imagen = pilas.imagenes.cargar_grilla("John_Cena_Completo.png" )
-        self.figura = pilas.fisica.Rectangulo(self.x, self.y, 65, 34,
-            friccion=0, restitucion=0)
+
     def actualizar(self):
-        self.figura.x=self.x
-        self.figura.y=self.y
+    	pilas.camara.x=self.x
+    	pilas.camara.y=self.y
 
         if pilas.control.abajo:
             self.imagen.avanzar()
             self.y -= 2
             self.rotacion=180
             self.espejado = True
-            self.figura.rotacion=180
 
 
         if pilas.control.arriba:
@@ -28,43 +28,38 @@ class JohnCena(pilasengine.actores.Actor):
             self.rotacion=0
             self.y += 2
             self.espejado = False
-            self.figura.rotacion=0
 
 
         if pilas.control.derecha:
             self.rotacion=-90
             self.imagen.avanzar()
             self.x +=2
-            self.figura.rotacion=-90
 
         if pilas.control.izquierda:
             self.rotacion=90
             self.imagen.avanzar()
             self.x -=2
-            self.figura.rotacion=90
 
 
 
         if pilas.control.izquierda and pilas.control.arriba:
             self.rotacion=45
-            self.figura.rotacion=45
+
         if pilas.control.izquierda and pilas.control.abajo:
             self.rotacion=135
-            self.figura.rotacion=135
         
         if pilas.control.derecha and pilas.control.arriba:
             self.rotacion=315
-            self.figura.rotacion=315
         
         if pilas.control.derecha and pilas.control.abajo:
             self.rotacion=225
-            self.figura.rotacion=225
           
 
-
+fisica_principal = pilas.fisica.Rectangulo(0,0,600,300,dinamica=False)
 
 pilas.actores.vincular(JohnCena)
 actor = pilas.actores.JohnCena()
+actor.aprender(pilas.habilidades.Imitar,fisica_principal)
 
 actor.escala=0.1
 

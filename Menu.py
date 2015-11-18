4bfee6460f6015a9f2f511b5bdbe6a2bd7c2207d
Juan Quintero <duke12lol@gmail.com>
2015-11-18 13:33:10 -0300

    correccion de errores

--- Menu.py
+++ Menu.py
@@ -4,7 +4,7 @@ import pilasengine
 pilas = pilasengine.iniciar()
 fondo = pilas.fondos.Fondo()
 fondo.imagen = pilas.imagenes.cargar('menu.jpg')
-fondo.escala=1.5
+fondo.escala=1.2
 sonido=pilas.actores.Sonido()
 intro = pilas.musica.cargar("mememe.mp3")
 intro.reproducir(repetir=True)
@@ -14,18 +14,22 @@ intro.reproducir(repetir=True)
 def iniciar_juego():
 	menu.eliminar()
 	intro.detener()
-<<<<<<< HEAD
-
+	fondo.eliminar()
 def Ayuda():
 	fondo.eliminar()
 	fondo3 = pilas.fondos.Fondo()
 	fondo3.imagen=pilas.imagenes.cargar('ayuda.jpg')	
 	fondo3.escala=1.2
 	menu.eliminar()
-=======
 	fondo.eliminar()
-	fondo2=pilas.fondos.Pasto()
->>>>>>> 5d9e02800b58b833a74c7343d71c91cb9bb07bbf
+	def Volver_al_menu():
+		menu2.eliminar()
+		fondo3.eliminar()
+	menu2=pilas.actores.Menu(
+        [
+            ('Volver', Volver_al_menu),
+        ])
+
 def salir_del_juego():
 	exit()
 
